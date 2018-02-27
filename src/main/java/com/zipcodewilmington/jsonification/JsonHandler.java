2798f560b7ebce87e7768eadb2d4acc54b505647
vvmk <vincentmasiello@gmail.com>
2018-02-27 14:39:33 -0500

    Un-re-desterilize master

---
+++ src/main/java/com/zipcodewilmington/jsonification/JsonHandler.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.jsonification;
+
+/**
+ * filename:
+ * project: JSONification
+ * author: https://github.com/vvmk
+ * date: 2/15/18
+ */
+public class JsonHandler {
+
+}
---
+++ src/test/java/com/zipcodewilmington/jsonification/JSONificationTest.java
@@ -0,0 +1,10 @@
+package com.zipcodewilmington.jsonification;
+
+/**
+ * filename:
+ * project: JSONificationTest
+ * author: https://github.com/vvmk
+ * date: 2/27/18
+ */
+public class JSONificationTest {
+}
